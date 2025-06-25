4a5004744550252161fdc80f37c4d2874cc8f051
letmein2 <85775286+letmein2@users.noreply.github.com>
2025-06-25 13:21:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,6 @@ LOG_DIR = "login_logs"
 os.makedirs(LOG_DIR, exist_ok=True)
 
 @app.route('/api/loglogin', methods=['POST'])
-# def log_login():
-#     data = request.get_json()
-#     username = data.get('username')
-#     computer = data.get('computerName')
-#     ip = data.get('publicIP')
-#     timestamp = data.get('timestamp', datetime.utcnow().isoformat())
-#     log_entry = f"{timestamp},{username},{computer},{ip}\n"
-#     log_file = os.path.join(LOG_DIR, f"{datetime.utcnow().date()}.log")
-#     with open(log_file, "a") as f:
-#         f.write(log_entry)
-#     return jsonify({"status": "success"}), 200
-
-######################################################
-
-
 def log_login():
     data = request.get_json()
     username = data.get('username')
